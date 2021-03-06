69ba49943878a6b10cb2893ee4b41dd4fb6b053e
yuji moriya <voyager.nasa0928@gmail.com>
2021-03-06 16:12:18 +0900

    新規作成画面および編集画面の作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,36 @@
 class TasksController < ApplicationController
 
-  before_action :set_task, only: [:show]
+  before_action :set_task, only: [:show, :edit, :update]
 
   def index
     @tasks = Task.all
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "作成しました！"
+    else
+      render :new
+    end
   end
 
   def show
   end
 
   def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "編集しました！"
+    else
+      render :edit
+    end
   end
 
   def destoy
