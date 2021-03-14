5401299995c8f254a5ab393180d93656c859510d
yuji moriya <voyager.nasa0928@gmail.com>
2021-03-14 16:58:54 +0900

    deploy調整

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,2 @@
 server '54.168.48.244', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/Users/yujimoriya/.ssh/id_rsa'
-# set :rails_env, 'production'
-# set :unicorn_rack_env, 'production'
